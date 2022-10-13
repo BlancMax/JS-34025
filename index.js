291224706848ad77c49bb3c90787bd2eaaeb0a4b
Blanc Luis Maximiliano <machys1985@gmail.com>
2022-10-13 19:03:12 -0300

    Subiendo proyecto 34025

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ document.addEventListener('DOMContentLoaded', () => {
         actualizarTotalesCarrito(carrito);
     };
 });
+const botonEliminar = document.getElementById ("eliminar");
+    botonEliminar.addEventListener('click', () => {
+        Swal.fire({
+            title: 'Está seguro de eliminar el producto?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Sí, seguro',
+            cancelButtonText: 'No, no quiero'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: 'Borrado!',
+                    icon: 'success',
+                    text: 'El archivo ha sido borrado'
+                })
+            }
+        })
+    })
